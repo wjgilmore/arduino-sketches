dcfc2edccd54e1a6491728bfbf9e687fec213831
Jason Gilmore <wj@wjgilmore.com>
2024-06-19 20:00:32 -0400

    First commit

---
+++ src/motors/basic.ino
@@ -0,0 +1,16 @@
+#include <Servo.h>
+
+int servoPin = 9;
+
+Servo myServo;
+
+void setup() {
+  myServo.attach(servoPin);
+}
+
+void loop() {
+  myServo.write(0);
+  delay(1000);
+  myServo.write(180);
+  delay(1000);
+}
